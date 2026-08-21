8fd14f1e15725a7c1be70f7e104af7fad144c539
Sparsh Gupta <sparshgupta10t@gmail.com>
2026-08-21 16:04:43 +0530

    Time: 0 ms (100%), Space: 87.2 MB (45.25%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int cnt5=0;
+        int cnt10=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5) cnt5++;
+            else{
+                int x = bills[i]-5;
+                if(x==5 && cnt5>=1){
+                    cnt5--;
+                    cnt10++;
+                }
+                else if(x==15){
+                    if(cnt10>=1 && cnt5>=1) {
+                        cnt10--;
+                        cnt5--;
+                    }
+                    else if(cnt5>=3){
+                        cnt5 = cnt5 - 3;
+                    }
+                    else return false;
+                } 
+                else return false;
+            }
+        }
+        return true;
+    }
+};
