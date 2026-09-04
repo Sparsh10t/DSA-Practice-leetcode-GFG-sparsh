4a37084024b74e5d98ec5ba75a563ad97be57361
Sparsh Gupta <sparshgupta10t@gmail.com>
2026-09-04 10:29:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (27.78%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n = s.size();
+        int low=0;
+        int high=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                low++;
+                high++;
+            }
+            else if(s[i]==')'){
+                low--;
+                high--;
+            }
+            else{
+                low--;
+                high++;
+            }
+            low = max(0,low);
+            if(high<0) return false;
+        }
+        if(low==0) return true;
+        return false;
+    }
+};
